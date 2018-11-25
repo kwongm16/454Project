8a9be6bb6d6d8149a481de2e49eeb1145fa70088
micha <micha@192.168.0.23>
2018-11-24 17:53:06 -0800

    Importing Data

--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ Created on Nov 24, 2018
 
 @author: micha
 '''
+#Importing Data from Line & Load Excel files
 from openpyxl import load_workbook
-wb2 = load_workbook(filename = '454_Data.xlsx')
-print(wb2.sheetnames)  
+lineDataFile = load_workbook(filename = 'Line_Data.xlsx')
+loadDataFile = load_workbook(filename = 'Load_Data.xlsx')
+
+#Testing printing features
+ws = lineDataFile.active
+cell_range = ws['A3':'E19'] 
+for row in cell_range.values: 
+    for value in row: 
+        print(value) 
+        
+
 
 
