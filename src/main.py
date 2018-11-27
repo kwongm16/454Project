618f139868a678258d5a93da8138ad1e0a851c66
micha <micha@10.19.46.89>
2018-11-26 23:11:24 -0800

    printing busData

--- src/main.py
+++ src/main.py
@@ -12,13 +12,21 @@ import cmath
 workbook = openpyxl.load_workbook('454 Data.xlsx') 
 
 #Making each sheet of data (Line & Load data) more accessable
-workbook.sheetnames
-lineData = workbook['Line Data'] 
-loadData = workbook['Load Data'] 
+lineData = workbook['LineData'] 
+busData = workbook['BusData'] 
+
+for row_index in range(1, (busData.max_row+1)): 
+    print ("Row:", row_index)
+    for col_index in range(1, (busData.max_column+1)): 
+        print ("Column:" , col_index, busData.cell(row=row_index, column=col_index).value)
+
+print(busData.max_row) 
+print(busData.max_column)
 
 print(lineData.max_row) 
-print(loadData.max_row) 
+print(lineData.max_column)
 
+print(lineData.cell(row = 2, column = 4).value)
 
 
 
